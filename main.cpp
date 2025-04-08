ae0fd0614b174a62449ddb890dd3666252cc5767
Jamster88 <mcfadden@auburn.edu>
2025-04-07 21:04:58 -0700

    added imports

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 //
 // Created by Jamster on 4/4/25.
 //
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <librdkafka/rdkafkacpp.h>
+#include <sys/inotify.h>
+#include <unistd.h>
+#include <stdexcept>
+#include <cstring>
+#include <errno.h>
+#include "FileMonitor.h"
 
 int main() {
     FileMonitor monitor("/path/to/your/file.txt", "localhost:9092", "your_kafka_topic");
